dc985ee2e6fffc07fd91eb19273ae19cbf90e094
jayampathiadhikari <jayampathiadhikari@gmail.com>
2019-11-12 02:12:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -17,13 +17,13 @@ $servername = "remotemysql.com";
     // $bprice = $_POST['bprice'];
     // $sprice = $_POST['sprice'];
 
-    $id = 10;
-    $name = 'jayx';
+    $id = 70;
+    $name = 'tfhgfhg';
     $brand = 'fuk';
     $bprice = 10;
     $sprice = 20;
 
-    $sql = "INSERT INTO products VALUES ($id, $name, $brand,$bprice,$sprice)";
+    $sql = "INSERT INTO products(ID,name,brand,buying_price,selling_price) VALUES ($id, $name, $brand,$bprice,$sprice)";
     if ($conn->query($sql) === TRUE) {
       echo 'Connected successfully';
         $details[]=array(
