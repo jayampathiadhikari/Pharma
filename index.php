7f72355f504c36db3cf01c70ee9dd28f96040eec
jayampathiadhikari <jayampathiadhikari@gmail.com>
2019-11-12 01:41:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,43 @@
-<?php ?>
+<?php 
+
+$servername = "remotemysql.com";
+    $username = "NUlBMh0F3u";
+    $password = "9ImvZnHsgZ";
+    $dbname = "NUlBMh0F3u";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+    }
+    
+    // $id = $_POST['id'];
+    // $name = $_POST['name'];
+    // $brand = $_POST['brand'];
+    // $bprice = $_POST['bprice'];
+    // $sprice = $_POST['sprice'];
+
+    $id = 10;
+    $name = 'jayx';
+    $brand = 'fuk';
+    $bprice = 'dsd';
+    $sprice = 'sdfdf';
+
+    $sql = "INSERT INTO products  (id, name, brand,bprice,sprice) VALUES ($id, $name, $brand,$bprice,$sprice)";
+    if ($conn->query($sql) === TRUE) {
+        $details[]=array(
+            'status'=>'success'
+        );
+        
+    } 
+    else {
+        $details[]=array(
+            'status'=>"Error updating record: " . $conn->error
+    );
+
+    };
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
